ed845f5ebdb365bcefa164026981a480c158ffc4
littlewilliam <tianchuang0205@gmail.com>
2016-03-27 16:08:23 +0800

    Initial commit

--- src/similarity/1_cos_T.py
+++ src/similarity/1_cos_T.py
@@ -1,15 +1,11 @@
 # coding=utf-8
 __author__ = 'tianchuang'
 
-import logging
-# logging.basicConfig(format='%(asctime)s: %(levelname)s : %(message)s', level=logging.INFO)
+# 将相似度矩阵 行与列转置
 
-from gensim import corpora, models, similarities
-import numpy as np
-import re
+
 import pandas as pd
 import time
-
 
 # python2.7 则需要以下转码
 # import sys
@@ -18,15 +14,14 @@ import time
 
 time_initial = time.time()
 
-cos_data = pd.read_csv('../../out/cos_all_A01B.csv',dtype={'industry': str})
+cos_data = pd.read_csv('../../out/cos_A_A01.csv', dtype={'industry': str})
 
 print(cos_data.head())
 
-cos_data_T=cos_data.T
+cos_data_T = cos_data.T
 
 print(cos_data_T.head())
 
-
-cos_data_T.to_csv('../../out/cos_A01B_all.csv')
+cos_data_T.to_csv('../../out/cos_A01_A.csv')
 
 print('数据转换完成! 耗时：%fs!' % (time.time() - time_initial))
